4acd7c7a607eeb13aa6382440507de38af1a849a
StruzhakS <StruzhakS@gmail.com>
2023-05-03 17:17:53 +0300

    task-1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-console.log(13);
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', startTimer);
+stopBtn.addEventListener('click', stopTimer);
+
+function startTimer() {
+  intervalId = setInterval(getBackgroundColorToBody, 1000);
+  startBtn.disabled = true;
+}
+
+function stopTimer() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+function getBackgroundColorToBody() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+console.log(21334);
